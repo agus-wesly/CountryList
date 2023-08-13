0b5957683a6784ea24effa55ffadc2cf8ab18749
aguswesly <agustchannel@gmail.com>
2023-08-13 12:24:27 +0700

    Deleting unused component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,6 @@ export default function Header({ children }: { children: ReactNode }) {
   return (
     <header className={style.header}>
       <h3>CountryList</h3>
-
       {children}
     </header>
   )
